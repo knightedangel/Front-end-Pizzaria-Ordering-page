bb13420e3a2d018706868ea36b3b6fef7b2dd8e5
Micah Innerarity <micah@Micahs-MacBook-Air.local>
2022-03-16 12:59:33 -0400

    intial

---
+++ script.js
@@ -0,0 +1,31 @@
+// Global variables
+let Crust = document.getElementById("crust");
+console.log(Crust); // Console logging as I go to ensure my code works.
+Crust = document.getElementById("crust").value
+console.log(Crust)
+
+
+
+// toppings array
+
+// event listener
+
+
+function calculateTotal(toppingArray) {  
+    let total = 0; 
+    let toppingCost = 2.50; 
+    let baseCost = 5.50; // cost of crust and sauce
+
+    // order string concatenation
+    
+
+    let toppingString = "Toppings: ";
+
+    // For Loop
+
+    // total = baseCost + cost of all toppings
+
+    // set DOM total += total   
+    // use DOM: = orderString   
+    // use DOM: = toppingString 
+}
